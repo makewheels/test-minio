4c06c439d83a6b005359d0de467844291059110f
makewheels <makewheels>
2021-11-13 01:04:44 +0800

    init

---
+++ src/main/java/com/example/testminio/TestMinioApplication.java
@@ -0,0 +1,13 @@
+package com.example.testminio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMinioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMinioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testminio/TestMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testminio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMinioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
