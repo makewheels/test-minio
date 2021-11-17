f3814c2da50e1e501ef11dbc5eca13ab4f093de0
makewheels <makewheels>
2021-11-17 10:11:22 +0800

    commit

--- src/main/java/com/example/testminio/FileUploader.java
+++ src/main/java/com/example/testminio/FileUploader.java
@@ -7,18 +7,33 @@ public class FileUploader {
     public static void main(String[] args) throws Exception {
         MinioClient minioClient =
                 MinioClient.builder()
-//                        .endpoint("http://minio.java8.icu:9000")
-                        .endpoint("https://oss-cn-beijing.aliyuncs.com")
-                        .credentials("LTAI5tJ9scBXxsk4VjYDiupv",
-                                "THckQsWudA7rNV0hUXn2Hcxu0VHhLC")
+                        //AWS
+//                        .region("ap-northeast-1")
+//                        .endpoint("https://s3.ap-northeast-1.amazonaws.com")
+//                        .credentials("AKIA3T7OC6BJUXVN3NXV", "z1HzN4QK8j8l0kT0H9uawxLnNO03zXv/tgRIfk23")
+
+                        //阿里云
+//                        .region("cn-beijing")
+//                        .endpoint("https://oss-cn-beijing.aliyuncs.com")
+//                        .credentials("LTAI5t8WesPNu1dDLaRmSWt8", "a45nM6ra07b203fXoZ7TKf3POJyYEi")
+
+                        //腾讯云
+                        .region("ap-beijing")
+                        .endpoint("https://cos.ap-beijing.myqcloud.com")
+                        .credentials("AKIDrYdYFhurY0DBwRggSzHL9hz3rPhjW4MP",
+                                "pOilY5MMyVlkSu50f4C4fgT1JL8fwxjG")
+
                         .build();
 
-
+        String objectName = System.currentTimeMillis() + ".txt";
         minioClient.uploadObject(
                 UploadObjectArgs.builder()
-                        .bucket("video-share-bucket")
-                        .object(System.currentTimeMillis() + ".pdf")
-                        .filename("C:\\Users\\thedoflin\\Downloads\\Sergio_Silva_License_Plate_Detection_ECCV_2018_paper.pdf")
+//                        .bucket("test-s3-bucket-tokyo")
+//                        .bucket("chat-oss-bucket")
+                        .bucket("bucket-1253319037")
+                        .object(objectName)
+                        .filename("C:\\Users\\thedoflin\\Downloads\\helloworld.txt")
                         .build());
+
     }
 }
